b7d648b062be576791f2c2516c14cfd0638e4eb5
zraccoon <jing.zdev3@gmail.com>
2022-09-15 00:29:03 -0700

    update

--- src/components/Basic/AccountModal.js
+++ src/components/Basic/AccountModal.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { initOnRamp } from '@coinbase/cbpay-js/dist/index.js';
+// import { initOnRamp } from '@coinbase/cbpay-js/dist/index.js';
+import { initOnRamp } from '@coinbase/cbpay-js';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Modal } from 'antd';
@@ -155,7 +156,7 @@ function AccountModal({ visible, onCancel, onDisconnect, settings }) {
 
   useEffect(() => {
     initOnRamp({
-      appId: 'your_app_id',
+      appId: process.env.REACT_APP_COINBASE_PAY_APP_ID,
       widgetParameters: {
         destinationWallets: [
           {
@@ -225,7 +226,7 @@ function AccountModal({ visible, onCancel, onDisconnect, settings }) {
           </div>
 
           <UserInfoButton>
-            <div className="item" onClick={() => { handleLink();}}>
+            <div className="item" onClick={() => { handleLink(); }}>
               View on Etherscan
               <svg viewBox="0 0 24 24" width="20px" xmlns="http://www.w3.org/2000/svg" class="sc-bdvvaa cpQaOW"><path d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"></path>
               </svg>
@@ -246,7 +247,7 @@ function AccountModal({ visible, onCancel, onDisconnect, settings }) {
           </UserInfoButton>
 
           <UserInfoButton>
-            <Button className="coinbase-pay-btn" onClick={() => { handleCoinbasePayClick(); }}>
+            <Button className="coinbase-pay-btn" disabled={!onrampInstance} onClick={() => { handleCoinbasePayClick(); }}>
               <img src={coinbaseImg} alt="coinbase" />Coinbase Pay
             </Button>
           </UserInfoButton>
