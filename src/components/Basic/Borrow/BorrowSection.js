0387024bfffdec24b6058e8866eb578959ddb006
zraccoon <andrei1120@outlook.com>
2023-07-04 07:37:59 -0700

    update

--- src/components/Basic/Borrow/BorrowSection.js
+++ src/components/Basic/Borrow/BorrowSection.js
@@ -204,9 +204,11 @@ function BorrowSection({ asset, settings, setSetting }) {
             </div>
             <span className="value green">
               {asset.borrowApy &&
-                getBigNumber(asset.borrowApy)
-                  .dp(2, 1)
-                  .toString(10)}
+                shortenNumberFormatter(
+                  getBigNumber(asset.borrowApy)
+                    .dp(2, 1)
+                    .toString(10)
+                )}
               %
             </span>
           </div>
